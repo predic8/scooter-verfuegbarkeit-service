35ebf7e6c68e0499ce34d9f5c816da22ca265e8f
Daniel Bonnauer <bonnauer@predic8.de>
2022-04-28 10:50:29 +0200

    added logger

--- src/main/java/de/predic8/scooter/VerfuegbarkeitsService.java
+++ src/main/java/de/predic8/scooter/VerfuegbarkeitsService.java
@@ -1,12 +1,17 @@
 package de.predic8.scooter;
 
 import de.predic8.scooter.model.Verfuegbarkeit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.lang.invoke.MethodHandles;
 import java.util.*;
 
 @Service
 public class VerfuegbarkeitsService {
+
+    private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     HashMap<String, Boolean> verfuegbarkeiten = new HashMap<>();
 
